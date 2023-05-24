af209e213d11a215c361c8c83ee97a7bfd9306cc
Taofeek F. Obafemi-Babatunde <taofeeko@microsoft.com>
2023-05-24 07:49:56 -0700

    revamping

--- src/Authentication/Authentication.Core/Utilities/AuthenticationHelpers.cs
+++ src/Authentication/Authentication.Core/Utilities/AuthenticationHelpers.cs
@@ -111,7 +111,7 @@ namespace Microsoft.Graph.PowerShell.Authentication.Core.Utilities
             if (authContext is null)
                 throw new AuthenticationException(ErrorConstants.Message.MissingAuthContext);
             InteractiveBrowserCredentialOptions interactiveOptions;
-            if (Microsoft.Graph.PowerShell.Authentication.GraphState.EnableWAMForMSGraph)
+            if (GraphSession.Instance.GraphOption.EnableWAMForMSGraph)
             {
                 [DllImport("user32.dll")] static extern IntPtr GetForegroundWindow();
                 IntPtr parentWindowHandle = GetForegroundWindow();
@@ -129,7 +129,7 @@ namespace Microsoft.Graph.PowerShell.Authentication.Core.Utilities
             {
                 AuthenticationRecord authRecord;
                 var interactiveBrowserCredential = new InteractiveBrowserCredential(interactiveOptions);
-                if (Microsoft.Graph.PowerShell.Authentication.GraphState.EnableWAMForMSGraph)
+                if (GraphSession.Instance.GraphOption.EnableWAMForMSGraph)
                 {
                     authRecord = interactiveBrowserCredential.Authenticate(new TokenRequestContext(authContext.Scopes), cancellationToken);
                 }
